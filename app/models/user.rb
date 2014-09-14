58025d59a76877186eb1f28d38dae4900a35cbb0
CF Grugan <charles_grugan@cable.comcast.com>
2014-09-14 14:23:42 -0400

    sttempting to fix production user issue that crashes the app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
+require Rails.root.join('app','models','user')
 class User < ActiveRecord::Base
   before_save {self.email = email.downcase}
   validates :name, presence: true, length: { maximum: 50}
