f8bc84eecf4be89737ca194ca82b065edc47a13a
CF <charles.grugan@gmail.com>
2014-10-05 13:10:22 -0400

    changing file location to help keep images from being destroyed on deployment

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,12 +1,12 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
   has_attached_file :photo, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-                    :url => "/system/:attachment/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
+                    :url => "/system/:class/:attachment/:id_partition/:style/:filename",
+                    :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
   validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
   has_attached_file :photo2, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png",
-                    :url => "/system/:attachment/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/system/:attachment/:id/:style/:basename.:extension"
+                    :url => "/system/:class/:attachment/:id_partition/:style/:filename",
+                    :path => ":rails_root/public/system/:class/:attachment/:id_partition/:style/:filename"
   validates_attachment_content_type :photo2, :content_type => /\Aimage\/.*\Z/
   validates :photo, presence: true
   validates :photo2, presence: true
