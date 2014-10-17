75f58f22aa201f20d2710623bf0bcca81aeaf08d
CF <charles.grugan@gmail.com>
2014-10-17 15:56:06 -0400

    removing unnecessary maps controller

--- app/controllers/map.rb
+++
@@ -1,7 +0,0 @@
-class MapController < ApplicationController
-  @users = User.all
-  @hash = Gmaps4rails.build_markers(@users) do |user, marker|
-    marker.lat micropost.latitude
-    marker.lng micropost.longitude
-  end
-end
